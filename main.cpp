481e0399bff59a440b79493e8f3de2d59619b5d7
yi-ge-dian <1085266008@qq.com>
2022-07-17 20:39:43 +0800

    灰度转换

--- main.cpp
+++ main.cpp
@@ -2,17 +2,33 @@
 // Created by 董文龙 on 2022/7/17.
 //
 #include <iostream>
+#include <opencv2/imgcodecs.hpp>
 #include <opencv2/opencv.hpp>
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
+#include "src/gray_level_transformation.h"
 
-int main() {
-    cv::Mat image = cv::imread("../test.jpg");
-    if (image.empty()){
+using namespace std;
+using namespace cv;
+
+int main(int argc, char **argv) {
+    // load image
+    string path ="../resources/img/test1.jpeg";
+    Mat image = imread(path);
+    if (image.empty()) {
         printf("Image not loaded");
         return -1;
     }
+
+    // call gray_trans function
+    float pixels[256];
+    for (int i = 0; i < 256; i++)
+        pixels[i] = powf(i, 1.5);   //
+    Mat imageLog;
+    gray_trans(image, imageLog, pixels);
+
+    //show image
     imshow("image", image);
-    cv::waitKey(0);
+    waitKey(0);
     return 0;
 }
---
+++ src/gray_level_transformation.h
@@ -0,0 +1,43 @@
+//
+// Created by 董文龙 on 2022/7/17.
+//
+
+#ifndef SPOT_DETECTION_GRAY_LEVEL_TRANSFORMATION_H
+#define SPOT_DETECTION_GRAY_LEVEL_TRANSFORMATION_H
+#include "opencv2/opencv.hpp"
+
+using namespace std;
+using namespace cv;
+
+void gray_trans(const Mat& src, Mat& dst,float* mapping)
+{
+    int channels = src.channels();
+    if (channels == 1)
+    {
+        dst = Mat(src.size(), CV_32FC1);
+        for (int i = 0; i < src.rows; i++)
+        {
+            float* p1 = dst.ptr<float>(i);
+            const uchar* p2 = src.ptr<uchar>(i);
+            for (int j = 0; j < src.cols; j++)
+                p1[j] = mapping[p2[j]];
+        }
+    }
+    else if (channels == 3)
+    {
+        dst = Mat(src.size(), CV_32FC3);
+        for (int i = 0; i < src.rows; i++)
+        {
+            float* p1 = dst.ptr<float>(i);
+            const uchar* p2 = src.ptr<uchar>(i);
+            for (int j = 0; j < src.cols * 3; j+=3)
+            {
+                p1[j] = mapping[p2[j]];
+                p1[j+1] = mapping[p2[j+1]];
+                p1[j+2] = mapping[p2[j+2]];
+            }
+        }
+    }
+}
+
+#endif //SPOT_DETECTION_GRAY_LEVEL_TRANSFORMATION_H
